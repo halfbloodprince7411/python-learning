a97d60d93db6e66cf70b963f58af75fa130af6cc
siddharth7411 <siddharth7411@gmail.com>
2025-06-16 13:14:08 -0400

    day1

---
+++ day1.py
@@ -0,0 +1,15 @@
+print("hello World")
+
+print("Hello world!\nHello World!")
+print("Hello world! "+"Hello World!")
+
+
+print("Hello "+input("whats your name?\n")+"!")
+
+print(len(input("What is your name?")))
+
+
+print('Welcome to the Band Name Generator.')
+city=input("What's the name of the city you grew up in?\n")
+pet_name=input("What's your pet name?\n")
+print("Your band name could be "+city+" "+pet_name)
