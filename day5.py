2a5cdf66038b0f9423acc9373dff34cce8103c31
siddharth7411 <siddharth7411@gmail.com>
2025-06-23 12:19:19 -0400

    day 5

---
+++ day5.py
@@ -0,0 +1,25 @@
+import random
+
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+print("Welcome to the PyPassword Generator!")
+nr_letters = int(input("How many letters would you like in your password?\n"))
+nr_symbols = int(input(f"How many symbols would you like?\n"))
+nr_numbers = int(input(f"How many numbers would you like?\n"))
+
+random_letters = random.sample(letters, nr_letters)
+random_symbols = random.sample(symbols, nr_symbols)
+random_numbers = random.sample(numbers, nr_numbers)
+
+print(random_letters)
+print(random_symbols)
+print(random_numbers)
+
+password=random_letters + random_symbols + random_numbers
+random.shuffle(password)
+for i in password:
+    print(i,end="")
+
+
